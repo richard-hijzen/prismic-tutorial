8e6c1a68bcb587526e7c84687acbe611e16304d1
Richard Hijzen <richardhijzen@hotmail.com>
2019-07-10 20:06:13 +0200

    gatsby-source-filesystem in comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
       },
     },
 
-    {
+    /*{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
-    },
+    },*/
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
