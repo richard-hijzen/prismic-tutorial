f60edc9c558bc05a16320305bfd139f3bce9be03
Richard Hijzen <richardhijzen@hotmail.com>
2019-07-16 20:20:59 +0200

    bugfix

--- src/components/slices/ImageGallery.js
+++ src/components/slices/ImageGallery.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Img from 'gatsby-image'
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/carousel'
 
 
 const ImageGallery = ({ slice }) => {
