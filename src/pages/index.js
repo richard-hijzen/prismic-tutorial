afb5238d52d999e100325827156fc111af3eab2c
Richard Hijzen <richardhijzen@hotmail.com>
2019-07-19 13:54:36 +0200

    added preload to styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -198,8 +198,8 @@ export default (props) => {
   return(
     <>
       <Helmet>
-            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+            <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
+            <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
       </Helmet>
       <PostBody homepage={ doc } />
       <Footer />
