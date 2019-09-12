8ba2f7d57b039beb292a2ebebf4a4f43403d1c40
Richard Hijzen <richardhijzen@hotmail.com>
2019-09-12 16:40:22 +0200

    added form to test

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -15,6 +15,26 @@ const Post = ({ data: { prismicPost } }) => {
       </header>
       <main id="blog-single-main">
         <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
+        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
       </main>
     </React.Fragment>
   )
