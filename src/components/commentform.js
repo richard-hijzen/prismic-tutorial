e05ee550837bf16d190dc0c59823a30e191e5567
Richard Hijzen <richardhijzen@hotmail.com>
2019-10-04 19:43:09 +0200

    added useEffect

--- src/components/commentform.js
+++ src/components/commentform.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { firebase } from '../firebase';
 
 function encode(data) {
@@ -10,21 +10,26 @@ function encode(data) {
 
 
 export default function CommentForm(props) {
-  const [state, setState] = React.useState({});
-  const id = props.id
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const id = props.id;
 
-  const handleChange = e => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+
+  useEffect(() => {
+    const id = props.id;
+    console.log({name}, {email}, {message});
+  });
+
 
     // Saves a new message on the Cloud Firestore.
     const saveMessage = (props) => {
-      const id = props.id
+      const id = props.id;
       // Add a new message entry to the Firebase database.
       return firebase.firestore().collection(`${props.id}`).add({
         name: `${name}`,
-        //email: {email},
-        //message: {message},
+        email: `${email}`,
+        message: `${message}`,
         //timestamp: firebase.firestore.FieldValue.serverTimestamp()
       }).catch(function(error) {
         console.error('Error writing new message to Firebase Database', error);
@@ -41,7 +46,7 @@ export default function CommentForm(props) {
         >
           <p>
             <label htmlFor="name">Your name:</label>
-            <input type="text" id="name" name="name" onChange={handleChange} />
+            <input type="text" id="name" name="name" onChange={(e) => setName(e.target.value)} />
           </p>
           <p>
             <label htmlFor="email">Your email:</label>
@@ -49,16 +54,16 @@ export default function CommentForm(props) {
               type="email"
               id="email"
               name="email"
-              onChange={handleChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </p>
           <p>
             <label htmlFor="textarea">Message:</label>
             <textarea
-              id="textarea"
+              id="message"
               name="message"
               rows="5"
-              onChange={handleChange}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </p>
           <p className="submit-button">
