0cdcb301702de723218e313940fdf233edc054d9
Richard Hijzen <richardhijzen@hotmail.com>
2019-10-09 17:13:02 +0200

    added comments

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,42 +1,55 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { firebase } from '../firebase';
 import { Time } from './date.js';
 
-export default function Comments(props) {
-  const id = props.id;
-
-  useEffect(() => {
-      LoadMessages();
-  });
-
-// Loads chat messages history and listens for upcoming ones.
-const LoadMessages = () => {
-    // Create the query to load the last 12 messages and listen for new ones.
-    var query = firebase.firestore()
-                    .collection(id)
-                    .orderBy('timestamp', 'desc')
-                    .limit(12);
-    
-    // Start listening to the query.
-    const messages = query.onSnapshot(function(snapshot) {
-      snapshot.docChanges().forEach(function(change) {
-          var message = change.doc.data();
-          return(
-                <div className="comment">
-                    <p>{message.time}</p>
-                    <p>{message.name}</p>
-                    <p>{message.email}</p>
-                    <p>{message.message}</p>
-                </div>
-          );
-        });
-    });
-    return messages;
+class Comments extends React.Component {
+  constructor(props) {
+    super(props);
+    const id = this.props.id;
+    this.state = {
+      comments: []
+    };
   }
 
-  return (
-    <>
-        <LoadMessages />
-    </>
-  )
-}
+  componentDidMount(props) {
+    const id = this.props.id;
+    firebase.firestore().collection(id)
+      .orderBy('timestamp', 'asc')
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        console.log(data);
+        this.setState({ comments: data });
+      });
+  }
+
+  render() {
+    const { comments } = this.state;
+    let hasComments;
+
+    if(comments.length > 0) {
+      hasComments = <div className="commentslist">
+                      {comments.map(comment => (
+                          <div key={comment.timestamp}>
+                                <div >
+                                  <h5>{comment.name}</h5>
+                                  <h6>{comment.message}</h6>
+                                  <p>{comment.time}</p>
+                                </div>
+                            </div>
+                      ))}
+                    </div>
+    } else {
+      hasComments = <p>No comment yet</p>
+    }
+    
+      return (
+        <>
+          {hasComments}
+        </>
+      );
+    }
+  }
+
+
+export default Comments;
