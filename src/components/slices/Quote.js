82313e3feac114c11d3e71d735894bc7da4d6e0a
Richard Hijzen <richardhijzen@hotmail.com>
2019-12-10 18:36:23 +0100

    added styling to quote slice

--- src/components/slices/Quote.js
+++ src/components/slices/Quote.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
+import "./quote.scss";
 
 export default ({ slice }) => (
   <div className="post-quote container">
-    <blockquote>{slice.primary.quote.text}</blockquote>
+    <blockquote>
+      <span className="quote-before">&#8220;</span>
+      {slice.primary.quote.text}
+      <span className="quote-after">&#x201d;</span>
+    </blockquote>
   </div>
 )
