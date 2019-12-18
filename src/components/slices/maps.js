7c5a60ff00deb316a8e07543a348591551e2a8f7
Richard Hijzen <richardhijzen@hotmail.com>
2019-12-18 18:00:22 +0100

    styled the map container

--- src/components/slices/maps.js
+++ src/components/slices/maps.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 export const MapContainer = (props) => {
+
+
     return (
-        <>
+        <div 
+            style={{width: '100%', height: '400px', position: 'relative'}}>
             <Map
                 google={props.google}
-                zoom={8}
+                className={'map'}
+                zoom={14}
                 initialCenter={{ lat: 51.9278891, lng: 4.3503239}}
             />
-        </>
+        </div>
     )
 }
 
