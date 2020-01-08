17c5f0a0ef01e8e3651005e8c345a74fefcc89f4
Richard Hijzen <richardhijzen@hotmail.com>
2020-01-08 17:48:31 +0100

    changed package

--- src/components/slices/maps.js
+++ src/components/slices/maps.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 export const MapContainer = (props) => {
     const lat = props.slice.primary.shop_location.latitude;
     const lng = props.slice.primary.shop_location.longitude
 
     return (
-        <div 
-            style={{width: '100%', height: '400px', position: 'relative'}}>
-            <Map
-                google={props.google}
-                className={'map'}
-                zoom={14}
-                initialCenter={{ lat: lat, lng: lng}}
+        <>
+            <LoadScript
+                id="script-loader"
+                googleMapsApiKey="AIzaSyAxuaMP4jaqjXgydBvlZ3Xq-cbKxvuvOvA"
             >
-                <Marker
-                    title={'The marker`s title will appear as a tooltip.'}
-                    name={'SOMA'}
-                    position={{lat: lat, lng: lng}} />
-            </Map>
-        </div>
+            <GoogleMap
+                id='example-map'
+                center={{lat: lat, lng: lng}}
+                zoom={10}
+            >    
+            </GoogleMap>
+            </LoadScript>
+        </>
     )
 }
 
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAxuaMP4jaqjXgydBvlZ3Xq-cbKxvuvOvA'
-  })(MapContainer);
+export default MapContainer;
